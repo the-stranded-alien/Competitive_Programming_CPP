8d5c9f45e329a0d97c41f99e2f2146a71a9d4e64
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-06 18:09:20 +0530

    Nth Fibonacci Number

---
+++ 05_Recursion/01_Basics/02_Nth_Fibonacci.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    // Base Case
+    if(n == 0 || n == 1) return n;
+    // Recursive Case
+    int f1 = fibonacci(n - 1);
+    int f2 = fibonacci(n - 2);
+    return (f1 + f2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+    return 0;
+}
