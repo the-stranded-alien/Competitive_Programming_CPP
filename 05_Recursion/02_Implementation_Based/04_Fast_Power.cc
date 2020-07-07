ba0db82e6909a34b27e3f8426865ed78d064e2bf
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-07 20:20:00 +0530

    Fast Power

---
+++ 05_Recursion/02_Implementation_Based/04_Fast_Power.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int power(int a, int n) {
+    if(n == 0) return 1;
+    return (a * power(a, n - 1));
+}
+
+int fast_power(int a, int n) {
+    if(n == 0) return 1;
+    // Recursive Case
+    // int smaller_ans = fast_power(a, n / 2) * fast_power(a, n / 2); // Inefficient -> More Rec. Call
+    int smaller_ans = fast_power(a, n / 2);
+    smaller_ans *= smaller_ans;
+    if(n & 1) return (smaller_ans * a);
+    return smaller_ans;
+}
+
+int main () {
+    int a, n;
+    cin >> a >> n;
+    cout << power(a, n) << endl;
+    cout << fast_power(a, n) << endl;
+    return 0;
+}
