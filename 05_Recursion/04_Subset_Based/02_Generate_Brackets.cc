ea00dd4399bef01cf41eb37cb3b46e804eaf35eb
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-09 14:21:33 +0530

    Generate Brackets

---
+++ 05_Recursion/04_Subset_Based/02_Generate_Brackets.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// Generate Balanced Brackets Using 'N' Pairs of Round Brackets.
+void generate_brackets(char *out, int n, int idx, int open, int close) {
+    // Base Case
+    if(idx == (2 * n)) {
+        out[idx] = '\0';
+        cout << out << endl;
+        return;
+    }
+    // Recursive Case
+    // 2 Conditions
+    if(open < n) {
+        out[idx] = '(';
+        generate_brackets(out, n, idx + 1, open + 1, close);
+    }
+    if(close < open) {
+        out[idx] = ')';
+        generate_brackets(out, n, idx + 1, open, close + 1);
+    }
+    return;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    char out[1000];
+    int idx = 0;
+    generate_brackets(out, n, idx, 0, 0);
+    return 0;
+}
