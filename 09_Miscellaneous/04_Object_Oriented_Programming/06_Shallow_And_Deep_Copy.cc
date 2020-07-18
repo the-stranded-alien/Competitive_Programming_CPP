7625622eb4d22793270c351bd466f04f758c131e
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-18 19:40:06 +0530

    Making Copy Constructor For Deep Copy

---
+++ 09_Miscellaneous/04_Object_Oriented_Programming/06_Shallow_And_Deep_Copy.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Car {
+private:
+    float price;
+public:
+    int model_num;
+    char *name;
+    Car() {
+        name = NULL;
+    }
+    Car(float p, int m, char *n) {
+        price = p;
+        model_num = m;
+        name = new char[strlen(n) + 1];
+        strcpy(name, n);
+    }
+    // Copy Constructor For Deep Copy
+    Car(Car &x) {
+        price = x.price;
+        model_num = x.model_num;
+        name = new char[strlen(x.name) + 1];
+        strcpy(name, x.name);
+    }
+    void set_price(int p) {
+        price = p;
+    }
+    void print() {
+        cout << "Name : " << name << endl;
+        cout << "Model Number : " << model_num << endl;
+        cout << "Price : " << price << endl;
+    }
+};
+
+int main() {
+    Car c(100, 200, "BMW"); // Parameterized Constructor
+    Car d(c); // Copy Constructor
+    d.set_price(400);
+    d.name[0] = 'A';
+    c.print();
+    d.print();
+    return 0;
+}
