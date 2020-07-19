88a1cc71e6c70d4e5a886303e70363fa552eb9a6
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-19 18:49:35 +0530

    Midpoint Runner Technique

---
+++ 08_Basic_Data_Structures/05_Linked_Lists/06_Midpoint.cc
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+class node {
+public: 
+    int data;
+    node *next;
+    // Constructor
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+// Passing Pointer Variable By Reference
+void insertAtHead(node *&head, int data) {
+    node *n = new node(data);
+    n->next = head;
+    head = n;
+}
+
+int length(node *head) {
+    int len = 0;
+    while(head != NULL) {
+        head = head->next;
+        len += 1;
+    }
+    return len;
+}
+
+void insertAtTail(node *&head, int data) {
+    if(head == NULL) {
+        head = new node(data);
+        return;
+    }
+    node *tail = head;
+    while(tail->next != NULL) tail = tail->next;
+    tail->next = new node(data);
+    return;
+}
+
+void print(node *head) {
+    // As Here, Head Is Passed By Value, Original Head Won't Change 
+    while(head != NULL) {
+        cout << head->data << " --> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void buildList(node *&head) {
+    int data;
+    cin >> data;
+    while(data != -1) {
+        insertAtTail(head, data);
+        cin >> data;
+    }
+}
+
+istream& operator>>(istream &is, node *&head) {
+    buildList(head);
+    return is;
+}
+
+ostream& operator<<(ostream &os, node *head) {
+    print(head);
+    return os;
+}
+
+// Runner Technique
+node *midPoint(node *head) {
+    if(head == NULL || head->next == NULL) return head;
+    node *slow = head;
+    node *fast = head->next;
+    while(fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+int main() {
+    node *head = NULL;
+    cin >> head;
+    cout << head;
+    node *m = midPoint(head);
+    cout << m->data << endl;
+    return 0;
+}
