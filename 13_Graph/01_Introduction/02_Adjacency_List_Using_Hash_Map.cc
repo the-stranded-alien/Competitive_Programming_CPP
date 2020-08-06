1e8b82e045c954a55644b431c2684cbb6ea4f1e6
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-06 19:27:32 +0530

    Weighted Graph - Adjacency List Using Hash-Map

---
+++ 13_Graph/01_Introduction/02_Adjacency_List_Using_Hash_Map.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<cstring>
+using namespace std;
+
+class Graph {
+    unordered_map<string, list<pair<string, int> > > l;
+public:
+    void addEdge(string x, string y, bool bidir, int wt) {
+        l[x].push_back(make_pair(y, wt));
+        if(bidir) l[y].push_back(make_pair(x, wt));
+    }
+    void printAdjList() {
+        // Iterate Over All The Keys In The Map
+        for(auto p : l) {
+            string src = p.first;
+            list<pair<string, int> > nbrs = p.second;
+            cout << "Source : " << src << " --> ";
+            for(auto nbr : nbrs) {
+                string dest = nbr.first;
+                int dist = nbr.second;
+                cout << "(" << dest << ", " << dist << ") ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph g;
+    g.addEdge("A", "B", true, 20);
+    g.addEdge("B", "D", true, 40);
+    g.addEdge("A", "C", true, 10);
+    g.addEdge("C", "D", true, 40);
+    g.addEdge("A", "D", false, 50);
+    g.printAdjList();
+    return 0;
+}
